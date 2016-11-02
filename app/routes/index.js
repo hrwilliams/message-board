b0d7d072485ddcc638b2a1ae7eb5419037061c7e
Halle Williams <williams.hr@gmail.com>
2016-11-02 09:30:05 -0700

    fix update functionality by fixing error in question-tile.js

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,6 +4,7 @@ export default Ember.Route.extend({
   model() {
     return this.store.findAll('question');
   },
+
   actions: {
    saveQuestion3(params) {
      var newQuestion = this.store.createRecord('question', params);
@@ -16,14 +17,14 @@ export default Ember.Route.extend({
         if(params[key]!==undefined) {
           question.set(key,params[key]);
         }
-      })
+      });
       question.save();
       this.transitionTo('index');
     },
 
-   destroyQuestion(question) {
-     question.destroyRecord();
-     this.transitionTo('index');
-   }
+  //  destroyQuestion(question) {
+  //    question.destroyRecord();
+  //    this.transitionTo('index');
+  //  }
  }
 });
