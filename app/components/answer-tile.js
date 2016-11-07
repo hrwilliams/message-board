effa747cd83e16125aa2cdd47b678da81e9688bf
Halle Williams <williams.hr@gmail.com>
2016-11-06 20:34:11 -0800

    add computed property for answers content and author

--- app/components/answer-tile.js
+++ app/components/answer-tile.js
@@ -1,4 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  heading: Ember.computed('answer.author', 'answer.content', function() {
+    return this.get('answer.content') + ' - ' + this.get('answer.author');
+  }),
 });
