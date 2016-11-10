b4187b6244c8b798f2338a95599bbfb8eea93274
Halle Williams <williams.hr@gmail.com>
2016-11-09 23:04:50 -0800

    display faves on detail page

--- app/routes/question.js
+++ app/routes/question.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  favoritesList: Ember.inject.service(),
   model(params) {
     return this.store.findRecord('question', params.question_id);
   },
